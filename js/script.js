97b57bc6d37e208ce2754bb89c883afee249a301
Sfreisem-Kirov <sfreisem-kirov@eastsideprep.org>
2018-01-07 23:18:35 -0800

    search function works
    for ymakas not kepahs

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    
+    for (i = 0; i < li.length; i++) {
+        h6 = li[i].getElementsByTagName("h6")[0];
+        console.log(h6);
+        if (h6.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+
+        }
+    }
+}
